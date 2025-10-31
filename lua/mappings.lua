1e83e82ab9f48ff7d7ba9b6b070f928bbdd6e7d1
lcx12901 <wktl1991504424@gmail.com>
2025-11-01 01:27:08 +0800

    feat: add lsp keybinds
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -68,10 +68,57 @@ wk.add {
 --! -- -- -- snacks -- -- -- !--
 map("n", "<leader>tl", "<cmd>lua Snacks.lazygit()<CR>", { desc = "Open lazygit" })
 
---! -- -- -- snacks -- -- -- !--
+--! -- -- -- nvim-session-manager -- -- -- !--
 wk.add { "<leader>S", desc = "Session Manager" }
 
 map("n", "<leader>SF", ":SessionManager load_session<CR>", { desc = "Load Session" })
 map("n", "<leader>SD", ":SessionManager delete_session<CR>", { desc = "Delete Session" })
 map("n", "<leader>SS", ":SessionManager save_current_session<CR>", { desc = "Save Session" })
 map("n", "<leader>SL", ":SessionManager load_last_session<CR>", { desc = "Load Last Session" })
+
+--! -- -- --  grug-far -- -- -- !--
+map("n", "<leader>rg", "<cmd>GrugFar<CR>", { desc = "GrugFar toggle" })
+
+--! -- -- --  nvim-lspconfig -- -- -- !--
+map("n", "<leader>cl", function()
+  Snacks.picker.lsp_config()
+end, { desc = "Lsp Info" })
+map("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
+map("n", "gr", vim.lsp.buf.references, { desc = "References", nowait = true })
+map("n", "gI", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
+map("n", "gy", vim.lsp.buf.type_definition, { desc = "Goto Type Definition" })
+map("n", "gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
+map("n", "K", function()
+  return vim.lsp.buf.hover()
+end, { desc = "Hover" })
+map("n", "gK", function()
+  return vim.lsp.buf.signature_help()
+end, { desc = "Signature Help" })
+map("i", "<c-k>", function()
+  return vim.lsp.buf.signature_help()
+end, { desc = "Signature Help" })
+map({ "n", "x" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+map({ "n", "x" }, "<leader>cc", vim.lsp.codelens.run, { desc = "Run Codelens" })
+map("n", "<leader>cC", vim.lsp.codelens.refresh, { desc = "Refresh & Display Codelens" })
+map("n", "<leader>cR", function()
+  Snacks.rename.rename_file()
+end, { desc = "Rename File" })
+map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
+map("n", "<leader>cA", function()
+  vim.lsp.buf.code_action { apply = true, context = {
+    only = { "source" },
+    diagnostics = {},
+  } }
+end, { desc = "Source Action" })
+map("n", "]]", function()
+  Snacks.words.jump(vim.v.count1)
+end, { desc = "Next Reference" })
+map("n", "[[", function()
+  Snacks.words.jump(-vim.v.count1)
+end, { desc = "Prev Reference" })
+map("n", "<a-n>", function()
+  Snacks.words.jump(vim.v.count1, true)
+end, { desc = "Next Reference" })
+map("n", "<a-p>", function()
+  Snacks.words.jump(-vim.v.count1, true)
+end, { desc = "Prev Reference" })
