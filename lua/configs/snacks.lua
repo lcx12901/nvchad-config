d5ddac62c68fb651163c034be8df08c893bb03ce
lcx12901 <wktl1991504424@gmail.com>
2025-11-09 19:08:53 +0800

    snacks: use picker
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- lua/configs/snacks.lua
+++ lua/configs/snacks.lua
@@ -10,6 +10,39 @@ local options = {
       git_hl = true,
     },
   },
+  picker = {
+    sources = {
+      noice = {
+        confirm = { "close" },
+      },
+    },
+    win = {
+      list = {
+        on_buf = function(self)
+          self:execute "calculate_file_truncate_width"
+        end,
+      },
+      preview = {
+        on_buf = function(self)
+          self:execute "calculate_file_truncate_width"
+        end,
+
+        on_close = function(self)
+          self:execute "calculate_file_truncate_width"
+        end,
+      },
+    },
+    layouts = {
+      select = {
+        layout = {
+          relative = "cursor",
+          width = 70,
+          min_width = 0,
+          row = 1,
+        },
+      },
+    },
+  },
 }
 
 return options
