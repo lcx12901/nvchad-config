c1de3a42d2aa0dfaeaedeec8bd723718ade776ac
lcx12901 <wktl1991504424@gmail.com>
2026-03-20 22:55:59 +0800

    chore(treesitter): modularize textobjects config

---
+++ lua/configs/treesitter-textobjects/opts.lua
@@ -0,0 +1,13 @@
+return {
+  move = {
+    enable = true,
+    set_jumps = true, -- whether to set jumps in the jumplist
+    -- LazyVim extention to create buffer-local keymaps
+    keys = {
+      goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+      goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+      goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+      goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+    },
+  },
+}
